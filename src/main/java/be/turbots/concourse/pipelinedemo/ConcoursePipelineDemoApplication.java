53bfb8a40d828a08173654266466c703ef8fe425
Dieter Hubau <dhubau@gmail.com>
2019-03-25 11:20:16 +0100

    Added Spring Boot skeleton files

---
+++ src/main/java/be/turbots/concourse/pipelinedemo/ConcoursePipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package be.turbots.concourse.pipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcoursePipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcoursePipelineDemoApplication.class, args);
+	}
+
+}
